89cd81fe5e9c222268c338f55a9b0a7154aab919
Diksi Erlangga <diksi131@gmail.com>
2021-10-11 17:11:13 +0800

    Attack and Jumping Animation
    added animations to player, including jumping and attacking, also fixed console UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,85 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MoveSpeed;
-    public Animator Animator;
-    private bool OnMove;
+    public float MoveSpeed, JumpingPower;
 
-    public Vector2 InputMovement;
+    public Animator Animator;
+    public Rigidbody2D RigidBody;
+    public Transform GroundCheck;
+    public LayerMask GroundLayer;
+
+    private bool IsFacingRight;
+
+    private float Horizontal;
+
+    private void Awake()
+    {
+        IsFacingRight = true;
+    }
 
     public void onMovement(InputAction.CallbackContext Value)
     {
-        InputMovement = Value.ReadValue<Vector2>();
-        OnMove = true;
+        Horizontal = Value.ReadValue<Vector2>().x;
     }
+
+    public void onJump(InputAction.CallbackContext Value)
+    {   
+        Debug.Log("Loncat");
+        if (IsGrounded())
+        {
+            RigidBody.velocity = new Vector2(RigidBody.velocity.x, JumpingPower);
+        }
+    }
+
+    public void onAttack(InputAction.CallbackContext Value)
+    {
+        Debug.Log("Nyerang");
+        Animator.SetTrigger("Attack");
+    }
+
     public void Update()
     {
-        if (OnMove)
-        {
-            Animator.SetFloat("Speed", Mathf.Abs(InputMovement.x * Time.deltaTime * MoveSpeed));
+        var velocity = RigidBody.velocity;
 
-            transform.position += new Vector3(InputMovement.x, InputMovement.y, 0) * Time.deltaTime * MoveSpeed;
+        RigidBody.velocity = new Vector2(Horizontal * MoveSpeed, RigidBody.velocity.y);
+
+        if(!IsFacingRight && Horizontal > 0f)
+        {
+            Flip();
         }
+        else if (IsFacingRight && Horizontal < 0f)
+        {
+            Flip();
+        }
+
+        //animasi loncat jatuh
+        else if (!IsGrounded() && RigidBody.velocity.y > 0){
+            Animator.SetTrigger("Jumping");
+        }
+        else if(!IsGrounded() && RigidBody.velocity.y < -2)
+        {
+            Animator.SetTrigger("Falling");
+        }
+
+        Animator.SetFloat("Speed", Mathf.Abs(Horizontal * Time.deltaTime * MoveSpeed));
+
+        transform.position += new Vector3(Horizontal, 0, 0) * Time.deltaTime * MoveSpeed;
+        //Debug.Log(transform.rotation);
+
+        //transform.Rotate(0, 180, 0 ,Space.Self);
+        //transform.rotation = Quaternion.Angle(transform.rotation, Angle);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position, 0.2f, GroundLayer);
+    }
+
+    private void Flip()
+    {
+        IsFacingRight        = !IsFacingRight;
+        Vector3 LocalScale   = transform.localScale;
+        LocalScale.x        *= -1f;
+        transform.localScale = LocalScale;
     }
 }
